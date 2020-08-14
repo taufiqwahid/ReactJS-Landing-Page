630080825468ca15956b59702aebc3b0b93f3f59
Taufiq Wahid <taufiq.wahid58@gmail.com>
2020-08-14 18:20:35 +0800

    Updates

---
+++ precache-manifest.99547f73997cdd608916cf477aaba47f.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36bdec2c6179a47983025a9bcce04f17",
+    "url": "/ReactJS-Landing-Page/index.html"
+  },
+  {
+    "revision": "8d00462894dcebf6e44b",
+    "url": "/ReactJS-Landing-Page/static/css/main.ed0b84c8.chunk.css"
+  },
+  {
+    "revision": "2bb0250ce46c73633422",
+    "url": "/ReactJS-Landing-Page/static/js/2.1f3fba6a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactJS-Landing-Page/static/js/2.1f3fba6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d00462894dcebf6e44b",
+    "url": "/ReactJS-Landing-Page/static/js/main.c1e7e423.chunk.js"
+  },
+  {
+    "revision": "7a61348cabe46afd4451",
+    "url": "/ReactJS-Landing-Page/static/js/runtime-main.045a6b62.js"
+  },
+  {
+    "revision": "b997820129b3ab5d80f5abd43161bd61",
+    "url": "/ReactJS-Landing-Page/static/media/clients-01.b9978201.svg"
+  },
+  {
+    "revision": "e54005bf054d03d33bd1b34a89fed3e7",
+    "url": "/ReactJS-Landing-Page/static/media/clients-02.e54005bf.svg"
+  },
+  {
+    "revision": "bbaa54a04b69de04ee674773e09302b1",
+    "url": "/ReactJS-Landing-Page/static/media/clients-03.bbaa54a0.svg"
+  },
+  {
+    "revision": "cdf0e500e4741076b6ee2e9787b4b445",
+    "url": "/ReactJS-Landing-Page/static/media/clients-04.cdf0e500.svg"
+  },
+  {
+    "revision": "b913b15068e1b11eb206a101eda47921",
+    "url": "/ReactJS-Landing-Page/static/media/clients-05.b913b150.svg"
+  },
+  {
+    "revision": "6dbc36133e254d171e27c7165309c05c",
+    "url": "/ReactJS-Landing-Page/static/media/feature-tile-icon-01.6dbc3613.svg"
+  },
+  {
+    "revision": "a02bfd4b6b5aa8cd6dc156002599a75a",
+    "url": "/ReactJS-Landing-Page/static/media/feature-tile-icon-02.a02bfd4b.svg"
+  },
+  {
+    "revision": "91b1b862e40b185edf9082c364243790",
+    "url": "/ReactJS-Landing-Page/static/media/feature-tile-icon-03.91b1b862.svg"
+  },
+  {
+    "revision": "687180834e71818255e449ec6ed473c0",
+    "url": "/ReactJS-Landing-Page/static/media/hero-image.68718083.png"
+  },
+  {
+    "revision": "4c66333f28a69b611caa3ab78e437a0b",
+    "url": "/ReactJS-Landing-Page/static/media/illustration-element-01.4c66333f.svg"
+  },
+  {
+    "revision": "ad9b81a8361d1ca5d6b49b80dd00035e",
+    "url": "/ReactJS-Landing-Page/static/media/illustration-section-01.ad9b81a8.svg"
+  },
+  {
+    "revision": "f062941c87f1c6075df1bcb7f49d85e4",
+    "url": "/ReactJS-Landing-Page/static/media/logo.f062941c.svg"
+  }
+]);
